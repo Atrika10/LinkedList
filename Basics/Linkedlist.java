875362ec3ce7bced94670f72f4f0e9df87390f6f
ATRIKA SHOW <readeratrika08@gmail.com>
2024-06-20 11:29:01 +0530

    isPalindrome Function

--- Basics/Linkedlist.java
+++ Basics/Linkedlist.java
@@ -186,19 +186,66 @@ public class Linkedlist {
         head =  prev;
     }
 
+    public static boolean isPalindrome(){
+        // cal size
+        int sz =0;
+        node temp = head;
+        while (temp != null) {
+            sz++;
+            temp = temp.next;
+        }
+
+        // find mid
+        int mid = sz/2;
+
+        // traverse till mid & reverse the first half
+        int i=0;
+        node prev = null;
+        node curr = head;
+        node next = null;
+
+        while (i < mid) {
+            // reverse the first half
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            i++;
+        }
+
+        // prev node is pointing to the last node of first half
+        // curr node is pointing to the first node of last half
+
+        // if size is odd
+        if(sz%2 !=0){
+            curr = curr.next;
+        }
+        
+        // Comparison step
+        while (prev != null && curr != null) {
+            if(prev.data != curr.data){
+                return false;
+            }else{
+                prev = prev.next;
+                curr = curr.next;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         Linkedlist ll = new Linkedlist();   // create an object of Linkedlist class
-        ll.addFirst(0);
-        ll.addLast(12);
-        ll.addLast(25);
-        ll.addLast(83);
-        ll.addLast(42);
-        ll.addLast(65);
+         ll.addFirst(0);
+         ll.addLast(12);
+         ll.addLast(25);
+         ll.addLast(83);
+         ll.addLast(42);
+         ll.addLast(65);
+         ll.printLL();
+         System.out.println(ll.searchKeyIdx(head, 0, 12));
+         ll.reverseLL();
+         ll.printLL();
 
-        ll.printLL();
-        System.out.println(ll.searchKeyIdx(head, 0, 12));
-        ll.reverseLL();
-        ll.printLL();
 
     }
     
